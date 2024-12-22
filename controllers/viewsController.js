22628084c6096210e5a53121c8d272296d4d5ee0
Christopher Torres <k.torres@seznam.cz>
2024-12-22 22:56:19 +0100

    it works no

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,18 @@
-const { performDNSLookup } = require('../services/dnsService');
+const { performDNSLookup, isIPAddress } = require('../services/dnsService');
+
+const dnsTypesGlobal = [
+  'A',
+  'AAAA',
+  'CAA',
+  'CNAME',
+  'MX',
+  'NAPTR',
+  'NS',
+  'PTR',
+  'SOA',
+  'SRV',
+  'TXT',
+];
 
 exports.getOverview = async (req, res) => {
   try {
@@ -19,15 +33,33 @@ exports.getDNSResults = async (req, res) => {
     const { domain } = req.query;
 
     if (!domain) {
-      return res.status(400).render('error', {
-        title: 'Error',
+      return res.status(400).json({
+        status: 'fail',
         message: 'Domain is required',
       });
     }
 
-    // Perform an ANY DNS lookup
-    const result = await performDNSLookup(domain, 'ANY');
+    // Check for IP address and filterS PTR
+    let dnsTypes = [];
+    if (!isIPAddress(domain)) {
+      dnsTypes = dnsTypesGlobal.filter((e) => e !== 'PTR');
+    } else {
+      dnsTypes = dnsTypesGlobal;
+    }
 
+    const result = {};
+
+    // Perform DNS lookup for all types in parallel
+    await Promise.all(
+      dnsTypes.map(async (type) => {
+        try {
+          result[type] = await performDNSLookup(domain, type);
+        } catch (error) {
+          result[type] = { error: error.message }; // Capture errors for specific types
+        }
+      }),
+    );
+    console.log(result);
     // Render results in a table
     res.status(200).render('result-page', {
       title: `DNS Lookup | ${domain}`,
